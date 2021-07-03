2f24bc6304241bb9e0d7330398c9f73029d937df
Cosmin Dinu <cdcdd15@gmail.com>
2021-07-03 11:33:37 +0300

    Modified endpoints urls.

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,7 +1,7 @@
 #clean my local docker environment
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
-docker rmi -f $(docker images -a -q)
+#docker rmi -f $(docker images -a -q)
 
 cd ..
 
@@ -9,5 +9,5 @@ cd ..
 mvn clean package
 
 #start containers
-docker run -d --network=spring-mysql-net -p 3306:3306 --name localmysql -e MYSQL_ROOT_PASSWORD=rootpass -e MYSQL_DATABASE=todos -e MYSQL_USER=cosmin -e MYSQL_PASSWORD=pass mysql
-docker run -d -p 5000:5000 --network=spring-mysql-net -e RDS_HOSTNAME=localmysql cosdin/raiffeisen-account-service:0.0.1-SNAPSHOT
+#docker run -d --network=spring-mysql-net -p 3306:3306 --name localmysql -e MYSQL_ROOT_PASSWORD=rootpass -e MYSQL_DATABASE=todos -e MYSQL_USER=cosmin -e MYSQL_PASSWORD=pass mysql
+#docker run -d -p 5000:5000 --network=spring-mysql-net -e RDS_HOSTNAME=localmysql cosdin/raiffeisen-account-service:0.0.1-SNAPSHOT
